e283658232d2746b16889f4cd6ee6a6b23d3d652
chromey85 <yayyub85@gmail.com>
2022-01-31 17:32:52 +0000

    edits

--- OOP_Tasks.js
+++ OOP_Tasks.js
@@ -1,14 +1,14 @@
-let carReg = SD04LNC;
+let carReg = "SD04LNC";
 let amountPaid = 10;
 let arrive = 800;
 let departure = 1200;
 
 class Cars{
         constructor(carReg, arrive, hCost) {
-            this. _carReg = carReg;
+            this._carReg = carReg;
             this._arrive = arrive;
             this._hCost = hCost;
-            this._paid = amountPaid;
+            // this._paid = amountPaid;
         }
         get carReg() {
             return this._carReg;
@@ -19,25 +19,30 @@ class Cars{
         get hCost() {
             return this._hCost;
         }
-        get amountPaid() {
-            return this._paid;
-        }
+        // get amountPaid() {
+        //     return this._paid;
+        // }
     };
 
-class carReg extends Cars {
-    constructor(carReg, departure){
-        super(carReg);
-        this._departureTime = departure;
-    }
+// class carReg extends Cars {
+//     constructor(carReg, departure){
+//         super(carReg);
+//         this._departureTime = departure;
+//     }
 
-    calculateCost() {
-        return (departure - this.arrive) * this.hCost;
-    }
+//     calculateCost() {
+//         return (departure - this.arrive) * this.hCost;
+//     }
     
-    makePayment() {
-        if (this.amountPaid >= this.calculateCost){
-            console.log("Parking fee has been paid")
-                } else {
-                    console.log("parking fee needs to be paid")}
-    }
-}
+//     makePayment() {
+//         if (this.amountPaid >= this.calculateCost){
+//             console.log("Parking fee has been paid")
+//                 } else {
+//                     console.log("parking fee needs to be paid")}
+//     }
+// }
+console.log(carReg)
+console.log(this._carReg)
+console.log(amountPaid)
+console.log(arrive)
+console.log(departure)
