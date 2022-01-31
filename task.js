5418872d0fd5b07d6ec08e6bf26c8e5c97750979
chromey85 <yayyub85@gmail.com>
2022-01-31 14:07:53 +0000

    edits

--- task.js
+++ task.js
@@ -53,3 +53,35 @@ class Cars{
 const carReg = new Cars("SD04LNC")
 
 console.log(carReg.hours*1.5)
+
+// // This is another example of how to do an OOP
+
+// class Car{
+//     constructor(regnum){
+//     this._regnum = regnum;
+//     this._hours = 0;
+//     this._charge = 0.00;
+//     }
+//     get regnum(){
+//     return this._regnum;
+//     }
+//     get hours(){
+//     return this._hours;
+//     }
+//     get charge(){
+//     return this._charge;
+//     }
+//     increaseHours(){
+//     this._hours++;
+//     this._charge += 1.50;
+//     }
+//    }
+//    const pay = (reg, hr) => {
+//     const car = new Car(reg);
+//     for (i = 0; i < hr; i++){
+//     car.increaseHours();
+//     }
+//     return `You need to pay £${car.charge} for ${car.hours} hours.`;
+//    }
+//    console.log(pay("M7 CAR", 5)); //Output: You need to pay £7.5 for 5 hours.
+
